4c4c04851ac530520a35c775181b4d1a6374b44e
Paweł Dynowski <pawel.dynowski@gmail.com>
2021-10-07 15:51:27 +0200

    figi improvements

--- api/figi.py
+++ api/figi.py
@@ -3,19 +3,16 @@ import requests
 
 
 class FIGI:
-    # def __init__(self):
-    #     self.figi_url: str = os.getenv("OPENFIGI_URL")
-    #     self.figi_key: str = os.getenv("OPENFIGI_KEY")
-    #     #self.headers: dict = {'Content-Type': 'text/json', 'X-OPENFIGI-APIKEY': self.figi_key}
+    def __init__(self):
+        self._api_key = os.environ.get("OPENFIGI_KEY")
+        self._url = os.environ.get("OPENFIGI_URL")
 
     def search_jobs(self, jobs: dict):
         headers = {
             "Content-Type": "text/json",
-            "X-OPENFIGI-APIKEY": os.environ.get("OPENFIGI_KEY"),
+            "X-OPENFIGI-APIKEY": self._api_key,
         }
-        response = requests.post(
-            url=os.environ.get("OPENFIGI_URL"), headers=headers, json=jobs
-        )
+        response = requests.post(url=self._url, headers=headers, json=jobs)
 
         if response.status_code != 200:
             raise Exception(f"Bad response code {response.status_code}")
